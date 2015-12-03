e182b25a42699c9f0abcc184527a1ed88bf7609c
Nick Davies <nbdavies@gmail.com>
2015-12-03 16:41:52 -0600

    Add 7.8 final state

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -1,4 +1,5 @@
 /*
+User story
 As a user, I want to be able to calculate the "sum" of a list of numbers.
 I aslo want to be able to calculate the "median" of a list of numbers.
 On top of that, I want to be able to calculate the "mean" (a.k.a. average) of a list of numbers.
@@ -7,3 +8,8 @@ These should all work whether my list has an even number of entries or not.
 And a pony. Or at least some kind of gift made out of a pony (and not too many monkeys). Please and thank you.
 */
 
+/* 
+Post-game Analysis
+The "mean" function didn't make it into the final code, but the "sum" and "median" functions passed all their tests. 
+We also made a pretty pony, which I had totally meant to just be a joke, but it's cool that it turned into something!
+*/
