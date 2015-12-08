d7872558a5c4bf6f8bf31e4a70e3b4a07decb1a3
Nick Davies <nbdavies@gmail.com>
2015-12-08 15:30:44 -0600

    Add 8.2

--- week-8/gradebook.js
+++ week-8/gradebook.js
@@ -1,8 +1,8 @@
 /*
 Gradebook from Names and Scores
 
-I worked on this challenge [by myself, with:]
-This challenge took me [#] hours.
+I worked on this challenge with: Jenna Espezua
+This challenge took me 0.5 hours.
 
 You will work with the following two variables.  The first, students, holds the names of four students.
 The second, scores, holds groups of test scores.  The relative positions of elements within the two
